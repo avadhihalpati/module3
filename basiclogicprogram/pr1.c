fb387667ae33cb47bb84bf212b55bf36f1eb088d
avadhihalpati <avadhihalpati1010@gmail.com>
2024-02-01 10:04:36 +0530

    Add files

---
+++ basiclogicprogram/pr1.c
@@ -0,0 +1,13 @@
+// Display This Information using printf
+//  a. Your Name
+// b. Your Birth date
+// c. Your Age
+// d. Your Address
+
+#include<stdio.h>
+int main(){
+	printf("Name:avadhi");
+	printf("\nBirthdate:10/03/2000");
+	printf("\nAge:23");
+	printf("\nAddress:chapra,navsari");
+}
---
+++ basiclogicprogram/pr3.c
@@ -0,0 +1,10 @@
+// WAP to Find Area And Circumference of Circle
+
+#include<stdio.h>
+int main(){
+	const PI=3.142;
+	int r;
+	float A;
+	A=(PI*r*r);
+	printf("area is %f",A);
+}
